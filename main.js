155af4f2716d7c87de9a8ef1b1129fa15dc95aaf
MVolodya <medvid.volodya@gmail.com>
2017-09-08 03:35:41 +0300

    added function: createForm

--- main.js
+++ main.js
@@ -7,34 +7,36 @@
         </form>
 */
 
+createForm();
 
-var body = document.querySelector('body');
+function createForm() {
+    var body = document.querySelector('body');
 
-var form = document.createElement('form');
-form.setAttribute('name', 'login');
-form.setAttribute('action', 'google.com');
+    var form = document.createElement('form');
+    form.setAttribute('name', 'login');
+    form.setAttribute('action', 'google.com');
 
-var inputAge = document.createElement('input');
-inputAge.setAttribute('type','text'); //inputAge.type='text';
-inputAge.setAttribute('name','age'); //inputAge.name='age';
-form.appendChild(inputAge);
+    var inputAge = document.createElement('input');
+    inputAge.setAttribute('type','text'); //inputAge.type='text';
+    inputAge.setAttribute('name','age'); //inputAge.name='age';
+    form.appendChild(inputAge);
 
-var inputUsername = document.createElement('input');
-inputUsername.setAttribute('type','text');
-inputUsername.setAttribute('name','username');
-form.appendChild(inputUsername);
+    var inputUsername = document.createElement('input');
+    inputUsername.setAttribute('type','text');
+    inputUsername.setAttribute('name','username');
+    form.appendChild(inputUsername);
 
-var inputDate = document.createElement('input');
-inputDate.setAttribute('type','text');
-inputDate.setAttribute('name','date');
-form.appendChild(inputDate);
+    var inputDate = document.createElement('input');
+    inputDate.setAttribute('type','text');
+    inputDate.setAttribute('name','date');
+    form.appendChild(inputDate);
 
-var inputSubmit = document.createElement('input');
-inputSubmit.setAttribute('type','submit');
-inputSubmit.setAttribute('value','Validate Me');
-form.appendChild(inputSubmit);
+    var inputSubmit = document.createElement('input');
+    inputSubmit.setAttribute('type','submit');
+    inputSubmit.setAttribute('value','Validate Me');
+    form.appendChild(inputSubmit);
 
-body.appendChild(form);
+    body.appendChild(form);
 
-
-console.log('form', form);
+    console.log('form', form);
+}
