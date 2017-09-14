ae0891a616540d9d60e8641e525af9b7e72dde06
MVolodya <medvid.volodya@gmail.com>
2017-09-14 05:53:38 +0300

    completed task3 with identification user's system

---
+++ js/task_3.js
@@ -0,0 +1,20 @@
+let navigatorMessageMap = {
+    'mac'   : 'macos',
+    'iphone': 'macos',
+    'win'   : 'windows'
+};
+
+let platform = navigator.platform.toLowerCase();
+let msg = document.getElementsByClassName('download-message');
+
+for (let key in navigatorMessageMap) {
+    key = key.toLowerCase();
+    let version = navigatorMessageMap[key];
+
+    if (platform.indexOf(key) >= 0){
+        for (let i = 0; i < msg.length; i++) {
+            let userPlatform = (msg[i].getAttribute('data-version') === version) ? true : false;
+            if (userPlatform) {msg[i].className = "active";}
+        }
+    }
+}
